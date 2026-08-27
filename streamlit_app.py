ce432156b018685306ecf99da57d39c90bab17b7
BBoyLoye <73763423+BBoyLoye@users.noreply.github.com>
2026-08-27 17:39:08 -0500

    Update streamlit_app.py
    Update to fix spelling error

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ st.write("The name on your smoothie will be: ", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.my_dataframe(data=my_Dataframe, use_container_width=True)
+st.my_dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
 
 ingredients_list = st.multiselect(
@@ -32,7 +32,7 @@ if ingredients_list:
         ingredients_string += fruit_chosen + ' '
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
-        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+        sf_df = st.my_dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
                     values ('""" + ingredients_string + """','"""+name_on_order+ """')"""
